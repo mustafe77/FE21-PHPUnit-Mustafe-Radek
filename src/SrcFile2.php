f30c3a52f629849aa3bde73794bc0828db27a374
Radek Slowinski <rotax@gmx.net>
2021-11-24 14:06:17 +0100

    extra exercise from serri added

---
+++ src/SrcFile2.php
@@ -0,0 +1,30 @@
+<?php
+namespace TDD;
+
+class SrcFile2  {
+   public $hostName = "localhost";
+   public $userName = "root";
+   public $password = "";
+   public $dbName = "codefactory";
+   public $conn;
+
+   function __construct(){
+     $this->connect();
+   }
+
+   private function connect(){
+     $this->conn = mysqli_connect($this->hostName, $this->userName, $this->password, $this->dbName);
+   }
+
+   public function insert($table, $fields,$values) {
+     $sql = "INSERT INTO $table ($fields) VALUES ($values)";
+     if($this->conn->query($sql)){
+          return "Success";
+     }else {
+          return "Error";
+     }
+   }
+
+}
+
+?>
